3ee2cceb21f05f22a8dd0ff0e41403897e3d3f4b
Navitha55 <navithapandu55@gmail.com>
2026-09-07 18:07:20 +0530

    Fix routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import "./index.css";
+
 import App from "./App.jsx";
+import "./index.css";
+
 import { store } from "./redux/store.js";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+
   <React.StrictMode>
-    <BrowserRouter basename="/NovaCart">
+
+    <HashRouter>
+
       <Provider store={store}>
+
         <ThemeProvider>
+
           <AuthProvider>
+
             <App />
+
           </AuthProvider>
+
         </ThemeProvider>
+
       </Provider>
-    </BrowserRouter>
+
+    </HashRouter>
+
   </React.StrictMode>
+
 );
